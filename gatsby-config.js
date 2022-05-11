a12e506975a1260d00812b01527824b84efef0a2
Daniel Gamero <whodatoj@gmail.com>
2022-05-11 09:47:57 -0700

    update char sheet and gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,5 +13,32 @@ module.exports = {
         `gatsby-transformer-csv`,
         'gatsby-plugin-sass',
         `gatsby-plugin-gatsby-cloud`,
+        {
+            resolve: `gatsby-plugin-google-gtag`,
+            options: {
+                // You can add multiple tracking ids and a pageview event will be fired for all of them.
+                trackingIds: [
+                    "G-Y7LQ3N7GXG", // Google Analytics / GA
+                ],
+                // This object gets passed directly to the gtag config command
+                // This config will be shared across all trackingIds
+                gtagConfig: {
+                    optimize_id: "GTM-K4R5NKQ",
+                    anonymize_ip: true,
+                    cookie_expires: 0,
+                },
+                // This object is used for configuration specific to this plugin
+                pluginConfig: {
+                    // Puts tracking script in the head instead of the body
+                    head: false,
+                    // Setting this parameter is also optional
+                    respectDNT: true,
+                    // Avoids sending pageview hits from custom paths
+                    //exclude: ["/preview/**", "/do-not-track/me/too/"],
+                    // Defaults to https://www.googletagmanager.com
+                    origin: "https://www.googletagmanager.com",
+                },
+            },
+        },
     ]
 }
