c50247ec67a6302b65682f392f267bf7b54edb74
yungd4nny <whodatoj@gmail.com>
2022-10-20 15:53:17 -0700

    added iframe for news

--- src/pages/newsPage.js
+++ src/pages/newsPage.js
@@ -1,30 +1,24 @@
 import * as React from 'react'
 import Header from '../components/Header.js';
 import { Container } from '../components/PageContainer.js';
+import ReactDOMServer from 'react-dom/server'
 
 function NewsPage() {
-    let news = getNews();
-
+    getNews();
     return (
         <Container>
             <Header></Header>
-            <h1>
-                Coming soon...
-            </h1>
+            <div style={{margin: "auto", width: "90vw", paddingTop: "30px"}}>
+                <iframe src={'https://api-us.ten-sura-m.wfs.games/web/announcement?region=1&language=2'}
+                style={{height: "80vh", width: "90vw", background: "gray", margin: "auto"}}></iframe>
+            </div>
         </Container>
     )
 }
 
 export default NewsPage
 
-async function getNews(){
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
-      
-    fetch("https://api-us.ten-sura-m.wfs.games/web/announcement?region=1&language=2&phoneType=1", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+async function getNews() {
+    let test = ReactDOMServer.renderToString(<iframe src={'https://api-us.ten-sura-m.wfs.games/web/announcement?region=1&language=2'}></iframe>);
+    console.log(test)
 }
